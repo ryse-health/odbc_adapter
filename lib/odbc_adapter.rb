f303b67e83f4258c2a58002e03c68a1d010375bd
aprescott <ash@aprescott.com>
2024-09-25 11:21:24 -0400

    Provide an adapter mapping for only Snowflake
    Any existing code that expects the pre-7.2 way of determining the
    adapter will assume that `adapter: "odbc"` is sufficient to pick the
    specific subclass (`SnowflakeODBCAdapter`, for instance), but that's no
    longer true. Until that code supports configuring the adapter name we'd
    rather just map everything to Snowflake, as that's our use case. Any
    other mappings only lead to mistakes, like accidentally using the
    `NullODBCAdapter`.

--- lib/odbc_adapter.rb
+++ lib/odbc_adapter.rb
@@ -4,7 +4,11 @@ require "odbc_adapter/adapters/mysql_odbc_adapter"
 require "odbc_adapter/adapters/postgresql_odbc_adapter"
 require "odbc_adapter/adapters/snowflake_odbc_adapter"
 
-ActiveRecord::ConnectionAdapters.register "odbc", "ODBCAdapter::Adapters::NullODBCAdapter", "odbc_adapter/adapters/null_odbc_adapter"
-ActiveRecord::ConnectionAdapters.register "mysql_odbc", "ODBCAdapter::Adapters::MySQLODBCAdapter", "odbc_adapter/adapters/mysql_odbc_adapter"
-ActiveRecord::ConnectionAdapters.register "postgresql_odbc", "ODBCAdapter::Adapters::PostgreSQLODBCAdapter", "odbc_adapter/adapters/postgresql_odbc_adapter"
+# The pre-7.2 approach to mapping adapter strings like "odbc" to an adapter
+# class meant that "odbc" would map to `ODBCAdapter`, which would then use
+# `DatabaseMetadata` to pick the correct adapter via `dbms_name`. The 7.2 way
+# involves a registered mapping, and thus `adapter: odbc` is not a dynamic
+# lookup. We do not need anything but Snowflake support, so preserve existing
+# `adapter: "odbc"` references as mapped to Snowflake.
+ActiveRecord::ConnectionAdapters.register "odbc", "ODBCAdapter::Adapters::SnowflakeODBCAdapter", "odbc_adapter/adapters/snowflake_odbc_adapter"
 ActiveRecord::ConnectionAdapters.register "snowflake_odbc", "ODBCAdapter::Adapters::SnowflakeODBCAdapter", "odbc_adapter/adapters/snowflake_odbc_adapter"
